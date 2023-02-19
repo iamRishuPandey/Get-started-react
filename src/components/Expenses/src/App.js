ee1185cc14b9f20bc8d2b3f7d0807aad691ebf3d
iamRishuPandey <111633192+iamRishuPandey@users.noreply.github.com>
2023-02-19 17:28:18 +0530

    updated

---
+++ src/components/Expenses/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const App = () => {
+  return (
+    <div>
+      <NewExpense />
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
